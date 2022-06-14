d43497a03639b347bf4af6cd131c352b81cb62e6
Ali <alifarajzade1@yahoo.com>
2022-06-14 18:05:52 +0430

    Create end-point for creating new hotel.

--- src/router/hotels.router.ts
+++ src/router/hotels.router.ts
@@ -1,5 +1,8 @@
 import { Router } from 'express'
+import * as hotelController from '../controllers/hotel.controller'
 
 const router = Router()
+
+router.route('/').post(hotelController.createNewHotel)
 
 export default router
