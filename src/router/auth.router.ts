74c497b0c3a045ef22b658f3301dc181e12ff5a7
Ali <alifarajzade1@yahoo.com>
2022-06-15 12:09:31 +0430

    Create end-point for singing up user.

--- src/router/auth.router.ts
+++ src/router/auth.router.ts
@@ -1,5 +1,8 @@
 import { Router } from 'express'
+import * as authController from '../controllers/auth.controller'
 
 const router = Router()
+
+router.route('/register').post(authController.registerNewUser)
 
 export default router
