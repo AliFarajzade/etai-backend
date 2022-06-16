1e8598867218fef8198baa532ed59502185f3655
Ali <alifarajzade1@yahoo.com>
2022-06-16 09:43:12 +0430

    Update user model.

--- src/models/user.model.ts
+++ src/models/user.model.ts
@@ -25,6 +25,10 @@ const UserSchema = new Schema(
             type: String,
             required: true,
         },
+        isAdmin: {
+            type: Boolean,
+            default: false,
+        },
     },
     { timestamps: true }
 )
