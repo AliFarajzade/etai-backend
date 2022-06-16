c2ab080f430f742b32e136a9a78a45e3e17a7d08
Ali <alifarajzade1@yahoo.com>
2022-06-16 12:39:32 +0430

    Add end-points for getting, deleting and updating rooms.

--- src/router/rooms.router.ts
+++ src/router/rooms.router.ts
@@ -1,5 +1,19 @@
 import { Router } from 'express'
+import * as roomController from '../controllers/room.controller'
+import { verifyAdmin, verifyToken } from '../middlewares/verify.middleware'
 
 const router = Router()
+
+router.route('/').get(roomController.getAllRooms)
+
+router
+    .route('/:hotelId')
+    .post(verifyToken, verifyAdmin, roomController.createNewRoom)
+
+router
+    .route('/:id')
+    .delete(verifyToken, verifyAdmin, roomController.deleteRoomById)
+    .patch(verifyToken, verifyAdmin, roomController.updateRoomById)
+    .get(roomController.getRoomById)
 
 export default router
