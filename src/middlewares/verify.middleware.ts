a665cce8cf635b23b83947eaadfbc4835008f51f
Ali <alifarajzade1@yahoo.com>
2022-06-16 11:16:03 +0430

    Change file and create middleware for checking authorization.

--- src/middlewares/verify.middleware.ts
+++ src/middlewares/verify.middleware.ts
@@ -21,3 +21,16 @@ export const verifyToken = (
         }
     )
 }
+
+export const verifyUser = (
+    req: Request,
+    _res: Response,
+    next: NextFunction
+) => {
+    if (
+        req.userCredentials?._id === req.params.id ||
+        req.userCredentials?.isAdmin
+    )
+        next()
+    else return next({ statusCode: 401, message: 'Unauthorized' })
+}
