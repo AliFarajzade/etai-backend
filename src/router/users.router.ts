b2077b08fe1b1daabdd91be91e5bf4c7fc0b68f4
Ali <alifarajzade1@yahoo.com>
2022-06-16 11:23:19 +0430

    Add new middlewares to users router.

--- src/router/users.router.ts
+++ src/router/users.router.ts
@@ -1,12 +1,13 @@
 import { Router } from 'express'
 import * as userController from '../controllers/user.controller'
+import { verifyToken, verifyUser } from '../middlewares/verify.middleware'
 
 const router = Router()
 
 router
     .route('/:id')
-    .patch(userController.updateUserById)
-    .delete(userController.deleteUserById)
+    .patch(verifyToken, verifyUser, userController.updateUserById)
+    .delete(verifyToken, verifyUser, userController.deleteUserById)
     .get(userController.getUserById)
 
 export default router
