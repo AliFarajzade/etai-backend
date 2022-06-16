69a16edb8cae3d3d542e5f68ef3462f87c17c779
Ali <alifarajzade1@yahoo.com>
2022-06-16 09:38:57 +0430

    Create end-point for logging users in.

--- src/router/auth.router.ts
+++ src/router/auth.router.ts
@@ -4,5 +4,6 @@ import * as authController from '../controllers/auth.controller'
 const router = Router()
 
 router.route('/register').post(authController.registerNewUser)
+router.route('/login').post(authController.logInUser)
 
 export default router
